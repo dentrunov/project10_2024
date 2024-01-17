50c6990a6b51b784f97326e0a6a8cf864c95e3c2
sgema <sgema@rambler.ru>
2024-01-17 08:48:28 +0300

    state fix

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "6933386159:AAFo7Sv8BHUasmzyXMyH-7vk0iRrTU6fgGg"
+
+smtp_server = "smtp.gmail.com"
+port = 587  # используйте порт 465 для SSL
+
+email = "your_email@gmail.com"
+password = "your_password"
